744a6e7a6c695d627bc63961dd51bf7b2958ace1
Ricardo Marín <147381654+RicJDev@users.noreply.github.com>
2025-09-17 15:46:01 -0400

    fix: corrigiendo ruta

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://ricjdev.github.io/',
+  base: '/gabocode-website'
 });
