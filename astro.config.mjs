baeb57332fb618d35c1a047dd056d2b411fd5b06
Ricardo Marín <147381654+RicJDev@users.noreply.github.com>
2025-09-17 15:56:15 -0400

    fix: intentando tactica pirata

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://ricjdev.github.io/',
-  base: '/gabocode-website'
+  site: 'https://ricjdev.github.io/gabocode-website'
 });
