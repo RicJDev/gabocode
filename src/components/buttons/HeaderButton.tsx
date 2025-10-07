0f5b3b8da1860131c594f675f0b423b551c6ec8d
Ricardo Marín <147381654+RicJDev@users.noreply.github.com>
2025-10-07 18:15:53 -0400

    feat: implementando boton en el header

--- src/components/buttons/HeaderButton.tsx
+++ src/components/buttons/HeaderButton.tsx
@@ -1,12 +1,14 @@
-import './HeaderButton.css'
+import './Button.css'
 import { useState } from 'react'
 import { IconMenu2, IconX } from '@tabler/icons-react'
 
 export function HeaderButton() {
   const [isOpen, setIsOpen] = useState(false)
+  const $mainNav = document.querySelector('.main-nav')
 
   function toggle() {
     setIsOpen(!isOpen)
+    $mainNav?.classList.toggle('openned')
   }
 
   return (
@@ -16,10 +18,10 @@ export function HeaderButton() {
         background: 'var(--background)',
         color: 'var(--accent)',
       }}
-
       onClick={toggle}
+      className="header-open-button"
     >
-      {isOpen ? <IconX className='header-icon' /> : <IconMenu2 className='header-icon' />}
+      {isOpen ? <IconX className="header-icon" /> : <IconMenu2 className="header-icon" />}
     </button>
   )
 }
